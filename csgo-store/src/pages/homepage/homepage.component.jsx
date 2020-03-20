6188963bb83756bf3ed805313e80dc4f7dea035d
Leonardo Rosa <leoabyss@gmail.com>
2020-03-20 09:24:26 -0300

    add menu item and effects, broken in small components

--- csgo-store/src/pages/homepage/homepage.component.jsx
+++ csgo-store/src/pages/homepage/homepage.component.jsx
@@ -1,40 +1,10 @@
 import React from "react";
 import "./homepage.styles.scss";
+import Directory from './../../components/directory/directory.component';
 
 const HomePage = () => (
   <div className="homepage">
-    <div className="directory-menu">
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title"> GUNS</h1>
-          <span className="subtitle"> SHOW NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title"> PISTOLS</h1>
-          <span className="subtitle"> SHOW NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title"> KNIFES</h1>
-          <span className="subtitle"> SHOW NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title"> CHARACTERS</h1>
-          <span className="subtitle"> SHOW NOW</span>
-        </div>
-      </div>
-      <div className="menu-item">
-        <div className="content">
-          <h1 className="title"> PREMIUM</h1>
-          <span className="subtitle"> SHOW NOW</span>
-        </div>
-      </div>
-    </div>
+    <Directory></Directory>
   </div>
 );
 
