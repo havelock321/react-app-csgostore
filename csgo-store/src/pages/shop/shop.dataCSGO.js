3d87ecc586287b6c9c468009c0e6a9b8cf896c93
Leonardo Silva <leoabyss@gmail.com>
2020-05-05 10:06:40 -0300

    Shop qty and new files

--- csgo-store/src/pages/shop/shop.dataCSGO.js
+++ csgo-store/src/pages/shop/shop.dataCSGO.js
@@ -1,174 +1,38 @@
 /* Used as starting data for ShopPage */
+import AK47 from "./cs images/CHAR00.png";
+import AWP from "./cs images/CHAR01.png";
+import M4A1 from "./cs images/CHAR02.png";
+import M4A12 from "./cs images/CHAR03.png";
 
 const SHOP_DATA = [
   {
     id: 1,
-    title: 'Rifles',
+    title: 'CONTRA TERRORISTA',
     routeName: 'rifles',
     items: [
       {
         id: 1,
-        name: 'AK-47',
-        imageUrl: "/images/shop-img/hats/blue-beanie.png",
-        price: 25
+        name: 'Ricksaw’s',
+        imageUrl: AK47,
+        price: 25000
       },
       {
         id: 2,
-        name: 'AWP',
-        imageUrl: "./images/shop-img/hats/blue-beanie.png",
-        price: 18
+        name: 'Buckshot',
+        imageUrl: AWP,
+        price: 18000
       },
       {
         id: 3,
-        name: 'M4A1-S',
-        imageUrl: "images/shop-img/hats/brown-cowboy.png",
+        name: 'FBI',
+        imageUrl: M4A1,
         price: 35
       },
       {
         id: 4,
-        name: 'M4A4',
+        name: 'Seal Team 6',
         // imageUrl: 'https://i.ibb.co/RjBLWxB/grey-brim.png',
-        imageUrl: "/images/shop-img/hats/grey-brim.png",
-        price: 25
-      }
-    ]
-  },
-  {
-    id: 2,
-    title: 'Facas',
-    routeName: 'facas',
-    items: [
-      {
-        id: 10,
-        name: 'Adidas NMD',
-        // imageUrl: 'https://i.ibb.co/0s3pdnc/adidas-nmd.png',
-        imageUrl: "/images/shop-img/sneakers/adidas-nmd.png", 
-        price: 220
-      },
-      {
-        id: 11,
-        name: 'Adidas Yeezy',
-        // imageUrl: 'https://i.ibb.co/dJbG1cT/yeezy.png',
-        imageUrl: "/images/shop-img/sneakers/yeezy.png",
-        price: 280
-      },
-      {
-        id: 12,
-        name: 'Black Converse',
-        // imageUrl: 'https://i.ibb.co/bPmVXyP/black-converse.png',
-        imageUrl: "/images/shop-img/sneakers/black-converse.png",
-        price: 110
-      },
-      {
-        id: 13,
-        name: 'Nike White AirForce',
-        // imageUrl: 'https://i.ibb.co/1RcFPk0/white-nike-high-tops.png',
-        imageUrl: "/images/shop-img/sneakers/white-nike-high-tops.png",
-        price: 160
-      }
-    ]
-  },
-  {
-    id: 3,
-    title: 'Pistolas',
-    routeName: 'pistolas',
-    items: [
-      {
-        id: 18,
-        name: 'Black Jean Shearling',
-        // imageUrl: 'https://i.ibb.co/XzcwL5s/black-shearling.png',
-        imageUrl: "/images/shop-img/jackets/black-shearling.png",
-        price: 125
-      },
-      {
-        id: 19,
-        name: 'Blue Jean Jacket',
-        // imageUrl: 'https://i.ibb.co/mJS6vz0/blue-jean-jacket.png',
-        imageUrl: "/images/shop-img/jackets/blue-jean-jacket.png",
-        price: 90
-      },
-      {
-        id: 20,
-        name: 'Grey Jean Jacket',
-        // imageUrl: 'https://i.ibb.co/N71k1ML/grey-jean-jacket.png',
-        imageUrl: "/images/shop-img/jackets/grey-jean-jacket.png",
-        price: 90
-      },
-      {
-        id: 21,
-        name: 'Brown Shearling',
-        // imageUrl: 'https://i.ibb.co/s96FpdP/brown-shearling.png',
-        imageUrl: "/images/shop-img/jackets/brown-shearling.png",
-        price: 165
-      }
-    ]
-  },
-  {
-    id: 4,
-    title: 'Terrorista',
-    routeName: 'terrorista',
-    items: [
-      {
-        id: 23,
-        name: 'Blue Tanktop',
-        // imageUrl: 'https://i.ibb.co/7CQVJNm/blue-tank.png',
-        imageUrl: "/images/shop-img/womens/blue-tank.png",
-        price: 25
-      },
-      {
-        id: 24,
-        name: 'Floral Blouse',
-        // imageUrl: 'https://i.ibb.co/4W2DGKm/floral-blouse.png',
-        imageUrl: "/images/shop-img/womens/floral-blouse.png",
-        price: 20
-      },
-      {
-        id: 25,
-        name: 'Floral Dress',
-        // imageUrl: 'https://i.ibb.co/KV18Ysr/floral-skirt.png',
-        imageUrl: "/images/shop-img/womens/floral-skirt.png",
-        price: 80
-      },
-      {
-        id: 26,
-        name: 'Red Dots Dress',
-        // imageUrl: 'https://i.ibb.co/N3BN1bh/red-polka-dot-dress.png',
-        imageUrl: "/images/shop-img/womens/red-polka-dot-dress.png",
-        price: 80
-      }
-    ]
-  },
-  {
-    id: 5,
-    title: 'Contra Terrorista',
-    routeName: 'contra_terrorista',
-    items: [
-      {
-        id: 30,
-        name: 'Camo Down Vest',
-        // imageUrl: 'https://i.ibb.co/xJS0T3Y/camo-vest.png',
-        imageUrl: "/images/shop-img/mens/camo-vest.png",
-        price: 325
-      },
-      {
-        id: 31,
-        name: 'Floral T-shirt',
-        // imageUrl: 'https://i.ibb.co/qMQ75QZ/floral-shirt.png',
-        imageUrl: "/images/shop-img/mens/floral-shirt.png",
-        price: 20
-      },
-      {
-        id: 32,
-        name: 'Black & White Longsleeve',
-        // imageUrl: 'https://i.ibb.co/55z32tw/long-sleeve.png',
-        imageUrl: "/images/shop-img/mens/long-sleeve.png",
-        price: 25
-      },
-      {
-        id: 33,
-        name: 'Pink T-shirt',
-        // imageUrl: 'https://i.ibb.co/RvwnBL8/pink-shirt.png',
-        imageUrl: "/images/shop-img/mens/pink-shirt.png",
+        imageUrl: M4A12,
         price: 25
       }
     ]
