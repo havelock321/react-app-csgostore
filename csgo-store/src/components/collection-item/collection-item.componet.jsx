43542a134ccf894a6e31e381c4996bcf431c9429
Leonardo Silva <leoabyss@gmail.com>
2020-05-06 10:14:18 -0300

    remove component

--- csgo-store/src/components/collection-item/collection-item.componet.jsx
+++ csgo-store/src/components/collection-item/collection-item.componet.jsx
@@ -5,9 +5,9 @@ import { connect } from "react-redux";
 import { addItem } from "../../redux/cart/cart.action";
 
 const CollectionItem = ({ item, addItem }) => {
-  const { name, price, imageUrl } = item;
+  const { name, price, imageUrl, isRifle } = item;
   return (
-    <div className="collection-item">
+    <div  className={`${isRifle ? "collection-item" : "collection-item"}`}>
       <div className="collection-header">
         <span className="name">{name}</span>
         <span className="price">${price}</span>
--- csgo-store/src/pages/shop/shop.dataCSGO.js
+++ csgo-store/src/pages/shop/shop.dataCSGO.js
@@ -1,39 +1,79 @@
 /* Used as starting data for ShopPage */
-import AK47 from "./cs images/CHAR00.png";
-import AWP from "./cs images/CHAR01.png";
+import RICK from "./cs images/CHAR00.png";
+import BUCK from "./cs images/CHAR01.png";
+import FBI from "./cs images/CHAR02.png";
+import SEAL from "./cs images/CHAR03.png";
+/* GUNS*/
+import AK47 from "./cs images/ak_preview.png";
+import AWP from "./cs images/awp_preview.png";
 import M4A1 from "./cs images/CHAR02.png";
 import M4A12 from "./cs images/CHAR03.png";
+
 
 const SHOP_DATA = [
   {
     id: 1,
     title: 'CONTRA TERRORISTA',
-    routeName: 'rifles',
+    routeName: 'ct',
     items: [
       {
         id: 1,
         name: 'Ricksaw’s',
-        imageUrl: AK47,
+        imageUrl: RICK,
         price: 25000
       },
       {
         id: 2,
         name: 'Buckshot',
-        imageUrl: AWP,
+        imageUrl: BUCK,
         price: 18000
       },
       {
         id: 3,
         name: 'FBI',
-        imageUrl: M4A1,
+        imageUrl: FBI,
         price: 35
       },
       {
         id: 4,
         name: 'Seal Team 6',
-        // imageUrl: 'https://i.ibb.co/RjBLWxB/grey-brim.png',
-        imageUrl: M4A12,
+        imageUrl: SEAL,
         price: 25
+      }
+    ]
+  },
+  {
+    id: 1,
+    title: 'RIFLES',
+    routeName: 'rifles',
+    items: [
+      {
+        id: 1,
+        name: 'AK47 CUSTOM',
+        imageUrl: AK47,
+        price: 25000,
+        isRifle: true
+      },
+      {
+        id: 1,
+        name: 'AK47 CUSTOM',
+        imageUrl: AK47,
+        price: 25000,
+        isRifle: true
+      },
+      {
+        id: 1,
+        name: 'AK47 CUSTOM',
+        imageUrl: AK47,
+        price: 25000,
+        isRifle: true
+      },
+      {
+        id: 1,
+        name: 'AK47 CUSTOM',
+        imageUrl: AK47,
+        price: 25000,
+        isRifle: true
       }
     ]
   }
